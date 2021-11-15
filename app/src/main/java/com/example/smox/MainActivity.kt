964e3ef6a5f299a12c673a79d58b2e52acd7d6fb
ParkAe-ri <parkeriimida00@gmail.com>
2021-11-15 20:27:24 +0700

    KELOMPOK 4

--- app/src/main/java/com/example/smox/MainActivity.kt
+++ app/src/main/java/com/example/smox/MainActivity.kt
@@ -7,31 +7,9 @@ private const val NUM_PAGES = 2
 
 class MainActivity : FragmentActivity() {
 
-    private lateinit var mPager: ViewPager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.history)
-        mPager = findViewById(R.id.pager)
-        val pagerAdapter = ScreenSlidePagerAdapter(supportFragmentManager)
-        mPager.adapter = pagerAdapter
+        setContentView(R.layout.confirm)
     }
-
-    override fun onBackPressed() {
-        if (mPager.currentItem == 0) {
-            super.onBackPressed()
-        } else {
-            mPager.currentItem = mPager.currentItem - 1
-        }
-    }
-
-    private inner class ScreenSlidePagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
-        override fun getCount(): Int = NUM_PAGES
-        override fun getItem(position: Int): Fragment = FragmentSatu()
-
-    }
-
-
-
-
 }
